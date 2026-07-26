a116db0d70ffd4700a1d1675498ae4341cf6395f
Matvey787 <118616823+Matvey787@users.noreply.github.com>
2026-07-26 15:05:10 +0300

    - The non-Docker build has been removed. - `paracl` is now set as a system variable. - Bug fixes.

--- compiler/install/install_windows.bat
+++ compiler/install/install_windows.bat
@@ -4,42 +4,19 @@ setlocal EnableDelayedExpansion
 set "INSTALL_DIR=%USERPROFILE%\.local\bin"
 if not exist "%INSTALL_DIR%" mkdir "%INSTALL_DIR%"
 
-call :check_dependencies
-if !errorlevel! neq 0 (
+echo The auto-installer for Windows only works through Docker.
 
-    echo Not all the required libraries for the build are installed on your OS. Required libraries: cmake, ninja-build, clang-21, llvm-21-dev, bison, flex, libspdlog-dev.
-    echo Let's try building it using Docker.
+call :install_with_docker
 
-    call :install_with_docker
-
-    if !errorlevel! equ 0 (
-        echo Compiler paracl successfully installed to %INSTALL_DIR%
-        exit /b 0
-    ) else (
-        echo Installation has failed.
-        exit /b 1
-    )
+if !errorlevel! equ 0 (
+    echo Compiler paracl successfully installed to %INSTALL_DIR%
+    exit /b 0
 ) else (
-    call :install_without_docker
-
-    if !errorlevel! equ 0 (
-        echo Compiler paracl successfully installed to %INSTALL_DIR%
-        exit /b 0
-    ) else (
-        echo Installation has failed.
-        exit /b 1
-    )
+    echo Installation has failed.
+    exit /b 1
 )
 
-
-:check_dependencies
-    where cmake >nul 2>nul || exit /b 1
-    where ninja >nul 2>nul || exit /b 1
-    where clang++ >nul 2>nul || exit /b 1
-    where bison >nul 2>nul || exit /b 1
-    where flex >nul 2>nul || exit /b 1
-    exit /b 0
-
+goto :EOF
 
 :install_with_docker
     where docker >nul 2>nul
@@ -54,16 +31,11 @@ if !errorlevel! neq 0 (
     (
     echo @echo off
     echo docker run -it -v "%%cd%%:/cwd" -w /cwd paracl %%*
-    ) > "%INSTALL_DIR%\paracl.bat"
+    ) > "%INSTALL_DIR%\paracl"
+
+    powershell -NoProfile -ExecutionPolicy Bypass -Command "$dir='%INSTALL_DIR%'; $p=[Environment]::GetEnvironmentVariable('Path', 'User'); if ($p -notlike ('*' + $dir + '*')) { [Environment]::SetEnvironmentVariable('Path', $p + ';' + $dir, 'User') }"
+
+    echo Please, reopen command line (power shell).
+
     exit /b !errorlevel!
 
-
-:install_without_docker
-    cmake -G Ninja -S compiler/ -B compiler/build -DCMAKE_CXX_COMPILER=clang++
-    if !errorlevel! neq 0 exit /b 1
-
-    cmake --build compiler/build
-    if !errorlevel! neq 0 exit /b 1
-
-    copy /Y "compiler\build\paracl" "%INSTALL_DIR%\"
-    exit /b !errorlevel!
