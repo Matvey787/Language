fb47d91c613bcfdf106f13083b2f59bd1ec7d866
Matvey787 <matveyklg@gmail.com>
2026-07-27 12:26:39 +0300

    Update tests

--- compiler/tests/unit/tests.cpp
+++ compiler/tests/unit/tests.cpp
@@ -9,10 +9,13 @@ const std::string exe_path_c   = "compiler/build/paracl";
 const std::string tests_path_c = "compiler/tests/";
 
 static std::string
-run(const std::filesystem::path& file)
+run(std::string_view file_name)
 {
-    auto&& exe_out = std::filesystem::path(file).replace_extension("exe");
-    auto&& ll_out  = std::filesystem::path(file).replace_extension("ll");
+    auto&& file =
+        std::filesystem::path(tests_path_c) / "e2e" / "in" / file_name;
+
+    auto exe_out = std::filesystem::path(file).replace_extension("exe");
+    auto ll_out  = std::filesystem::path(file).replace_extension("ll");
 
     std::string compile_cmd =
         std::format("{} {} -o {}", exe_path_c, file.string(), exe_out.string());
@@ -46,9 +49,11 @@ run(const std::filesystem::path& file)
 }
 
 static std::string
-read(const std::filesystem::path& path)
+read(const std::filesystem::path& file_name)
 {
-    std::ifstream f(path);
+    auto file = std::filesystem::path(tests_path_c) / "e2e" / "out" / file_name;
+
+    std::ifstream f(file);
     std::ostringstream ss;
 
     ss << f.rdbuf();
@@ -58,24 +63,12 @@ read(const std::filesystem::path& path)
 
 TEST(E2E, FunctionsDefaultArgs)
 {
-    auto&& in =
-        std::filesystem::path(tests_path_c) / "e2e" / "in" / "test1.myl";
-
-    auto&& out =
-        std::filesystem::path(tests_path_c) / "e2e" / "out" / "test1.ans";
-
-    EXPECT_EQ(run(in), read(out));
+    EXPECT_EQ(run("test1.myl"), read("test1.ans"));
 }
 
 TEST(E2E, ScopedVariables)
 {
-    auto&& in =
-        std::filesystem::path(tests_path_c) / "e2e" / "in" / "test2.myl";
-
-    auto&& out =
-        std::filesystem::path(tests_path_c) / "e2e" / "out" / "test2.txt";
-
-    EXPECT_EQ(run(in), read(out));
+    EXPECT_EQ(run("test2.myl"), read("test2.ans"));
 }
 
 int
